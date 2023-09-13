58bb2cc8d104141032c765a4239f52180dee07e3
AG0n1 <99075096+AG0n1@users.noreply.github.com>
2023-09-13 07:15:13 +0300

    Add files via upload

---
+++ hackathon-proj/assets/main.js
@@ -0,0 +1,16 @@
+/*
+// Получаем ссылку на элемент, который будет следовать за курсором
+const follower = document.getElementsByClassName('pill');
+// Добавляем обработчик события "mousemove" на document
+document.addEventListener('DOMContentLoaded', () => {
+    // Ваш код для следования за курсором
+    const follower = document.getElementsByClassName('pill');
+
+    document.addEventListener('mousemove', (e) => {
+        follower.style.left = e.clientX + 'px';
+        follower.style.top = e.clientY + 'px';
+    });
+});
+
+*/
+
